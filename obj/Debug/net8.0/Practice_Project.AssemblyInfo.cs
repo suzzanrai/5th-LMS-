1d6ae4bb5b0a1dc945aa87ff0a06176a303fe279
suzzanrai <163089825+suzzanrai@users.noreply.github.com>
2026-06-23 13:04:19 +0545

    merge with feature branch, this is the new folder that i create new repo and work:

--- obj/Debug/net8.0/Practice_Project.AssemblyInfo.cs
+++ obj/Debug/net8.0/Practice_Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Practice_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7dc8363cb38b2ffd7c09bf793362f6e2200d7b7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a560cf90c3b704c8446c34361291c2f760a19091")]
 [assembly: System.Reflection.AssemblyProductAttribute("Practice_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Practice_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
